371e547bcdba5653087d5e2731f478c12ea0eafb
Laura Code <laujcv12@gmail.com>
2025-03-03 18:59:25 -0500

    Hice un carrito de compras sencillo

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react"
+import { CardComponent } from "../components/CardComponent"
+import { ProductContext } from "../context/ProductContext"
+import { CartContext } from "../context/CartContext"
+
+export const ProductsPage = () => {
+
+    const { products } = useContext(ProductContext)
+
+    const { addProduct, removeProduct } = useContext(CartContext)
+
+
+
+    return (
+        <>
+            <h1>Productos</h1>
+            <hr />
+            {products.map(product => (
+                < CardComponent
+                    id={product.id}
+                    key={product.id}
+                    image={product.image}
+                    title={product.title}
+                    description={product.description}
+                    price={product.price}
+                    handlerAdd={() => addProduct(product)}
+                    handlerRemove={() => removeProduct(product.id)}
+                />
+            )
+            )
+            }
+        </>
+    )
+}
